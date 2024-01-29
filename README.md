99c812031e78481c8393f915f8a165a8d3059c20
Eric Williams <ewilliams0305@gmail.com>
2024-01-29 07:23:30 -0500

    added source

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# ndc_event_sourced
-NDC Event Sourced Systems
+# NDC London 2024 Workshop - Building an event sourced system in C#
+
+This repo is where we will share code and slides for what we do during the 2 day workshop.
---
+++ source/BeerSender/BeerSender.Domain/Boxes/Shipping.cs
@@ -0,0 +1,26 @@
+﻿namespace BeerSender.Domain.Boxes;
+
+public record ShippingAddress(int Number, string Street, int PostalCode);
+
+public record CreateShippingLabel(ShippingAddress Address);
+
+public record LabelWasInvalid(ShippingLabel Label);
+
+public record LabelWasApplied(ShippingAddress Label, Box Box);
+
+public class ShippingLabel
+{
+    public ShippingAddress Address { get; private set; }
+    
+    public ShippingLabel(ShippingAddress address)
+    {
+        Address = address;
+    }
+}
+
+
+public record ShipBox(Box BoxToShip, ShippingLabel Label);
+
+public record BoxShipped(Box ShippedBox);
+
+public record BoxNotReadyToShip(Box NotReadyBox);
